36a454ba6f225b3604b2f5f8f3c320b8e683f165
Irene Liao <iireneliao@gmail.com>
2023-04-21 10:28:04 -0700

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Irene Liao on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
