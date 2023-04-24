a08469fdb52e1826fb955c3d23f22575afaa5aa3
Irene Liao <iireneliao@gmail.com>
2023-04-23 21:56:32 -0700

    initial commit

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageName = "me";
+    @State private var txt = "Hi, I'm Irene :)"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack() {
+            Text("Hello!")
+                .font(.title)
+                .padding(.bottom)
+            Text("My name is Irene Liao")
+                .font(.title3)
+                .multilineTextAlignment(.center)
+            Text("and I want to conquer all the different fonts of skating")
+                .multilineTextAlignment(.center)
+            Text("Click below to find out what I'm talking about!")
+                .padding(.top)
+            Button("Click me!") {
+                self.imageName = "hobbies"
+                self.txt = "❄️ skating, 🌎 skating, 💧 skating"
+            }
+                .buttonStyle(.borderedProminent)
+                .tint(Color(hue: 0.763, saturation: 0.314, brightness: 0.951))
+                .padding(.bottom)
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.top)
+            Text(txt)
         }
         .padding()
     }
